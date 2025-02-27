6e985f42bdb77dc7b243b1837b28270dd9b42274
Edward <https://phamanh.157.mta@gamil.com>
2025-02-27 12:28:35 +0700

    add transform for dataframe

--- dataframes/external.go
+++ dataframes/external.go
@@ -47,6 +47,45 @@ func AsType[S common.Frame, D common.Frame](sourceDf *DataFrame[S]) (*DataFrame[
 	}, nil
 }
 
+func Transform[S common.Frame, D common.Frame](sourceDf *DataFrame[S], fn func(*S) *D) (*DataFrame[D], error) {
+	if sourceDf == nil {
+		return nil, errors.New("sourceDf must not be nil")
+	}
+
+	var seriesDf []*series.Series[D]
+
+	seriesDf = lo.Map(sourceDf.Series, func(s *series.Series[S], index int) *series.Series[D] {
+		valSer := lo.Map(s.Values, func(val *S, index int) *D {
+			return fn(val)
+		})
+		result := &series.Series[D]{
+			Name:    s.Name,
+			Indexes: s.Indexes,
+			Values:  valSer,
+			DType:   s.DType,
+		}
+		result.UpdateDType()
+		return result
+	})
+
+	var dfValues [][]*D
+	if sourceDf.Values != nil && len(sourceDf.Values) > 0 {
+		dfValues = lo.Map(sourceDf.Values, func(row []*S, index int) []*D {
+			return lo.Map(row, func(val *S, index int) *D {
+				return fn(val)
+			})
+		})
+	}
+
+	return &DataFrame[D]{
+		Indexes: sourceDf.Indexes,
+		Columns: sourceDf.Columns,
+		DType:   sourceDf.DType,
+		Values:  dfValues,
+		Series:  seriesDf,
+	}, nil
+}
+
 func Apply[S common.Frame, D common.Frame](s *DataFrame[S], fn func(*DataFrame[S]) *DataFrame[D]) (*DataFrame[D], error) {
 	if s == nil {
 		return nil, errors.New("source go-dataframe is nil")
